2de41b23d872916107913ad5d884fedb5adc3c55
Ryan O'Connor <me@ryanoconnor.dev>
2020-04-20 08:13:20 -0400

    no message

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,28 +4,28 @@ module.exports = {
     title: `Ryan O'Connor`,
     author: {
       name: `Ryan O'Connor`,
-      summary: `New York`,
+      summary: `New York`
     },
     description: `Developer - Designer - Media`,
     siteUrl: `https://ryanoconnor.dev`,
     social: {
-      twitter: `ryan7oconnor`,
-    },
+      twitter: `ryan7oconnor`
+    }
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
+        name: `blog`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
+        name: `assets`
+      }
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -34,33 +34,35 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
-            },
+              maxWidth: 590
+            }
           },
           {
             resolve: `gatsby-remark-responsive-iframe`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
+              wrapperStyle: `margin-bottom: 1.0725rem`
+            }
           },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
           `@pauliescanlon/gatsby-remark-sticky-table`,
-          {resolve: "gatsby-remark-custom-blocks",
-          options: {
-            blocks: {
-              danger: {
-                classes: "danger",
-              },
-              img25: {
-                classes: "img25",
-                title: "optional"
-              },
-            },
-          },}
-        ],
-      },
+          {
+            resolve: "gatsby-remark-custom-blocks",
+            options: {
+              blocks: {
+                danger: {
+                  classes: "danger"
+                },
+                img25: {
+                  classes: "img25",
+                  title: "optional"
+                }
+              }
+            }
+          }
+        ]
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -73,22 +75,21 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
+        icon: `content/assets/gatsby-icon.png`
+      }
     },
     {
       resolve: `gatsby-plugin-typescript`,
       options: {
         isTSX: true, // defaults to false
-        allExtensions: true, // defaults to false
-      },
+        allExtensions: true // defaults to false
+      }
     },
     `gatsby-plugin-react-helmet`,
-      'gatsby-plugin-debug-build',
-    
-    
+    "gatsby-plugin-debug-build"
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
+  ]
 }
